a249841e551698cfe5bf841763bc84b50e9ba709
Gusarov Evgeny <p3gus@yandex.ru>
2019-07-07 18:53:21 +0300

    Edited UIconfig for production

--- web/vue/dist/UIconfig.js
+++ web/vue/dist/UIconfig.js
@@ -4,17 +4,25 @@
 // This config is used by both the frontend as well as the web server.
 // see https://gekko.wizb.it/docs/installation/installing_gekko_on_a_server.html#Configuring-Gekko
 
+SSL = true;
+
+if(typeof window === 'undefined')
+  PORT = process.env.PORT;
+else
+  PORT = SSL ? 443 : 80;
+
+
 const CONFIG = {
-  headless: false,
+  headless: true,
   api: {
-    host: '127.0.0.1',
-    port: 3000,
+    host: '0.0.0.0',
+    port: PORT,
     timeout: 120000 // 2 minutes
   },
   ui: {
-    ssl: false,
-    host: 'localhost',
-    port: 3000,
+    ssl: SSL,
+    host: 'shrouded-escarpment-61081.herokuapp.com',
+    port: PORT,
     path: '/'
   },
   adapter: 'sqlite'
